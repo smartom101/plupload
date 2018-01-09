e3941db449ed3471f58dc0712b96c49c0bf933be
汤坤 <tangkun@yingwumeijia.com>
2018-01-09 17:57:06 +0800

    no fliter

--- js/moxie.js
+++ js/moxie.js
@@ -7172,10 +7172,14 @@ define("moxie/runtime/html5/file/FileInput", [
 
 				shimContainer = I.getShimContainer();
 
-				shimContainer.innerHTML = '<input id="' + I.uid +'" type="file" style="font-size:999px;opacity:0;"' +
-					(_options.multiple && I.can('select_multiple') ? 'multiple' : '') + 
-					(_options.directory && I.can('select_folder') ? 'webkitdirectory directory' : '') + // Chrome 11+
-					(mimes ? ' accept="' + mimes.join(',') + '"' : '') + ' />';
+				// shimContainer.innerHTML = '<input id="' + I.uid +'" type="file" style="font-size:999px;opacity:0;"' +
+				// 	(_options.multiple && I.can('select_multiple') ? 'multiple' : '') +
+				// 	(_options.directory && I.can('select_folder') ? 'webkitdirectory directory' : '') + // Chrome 11+
+				// 	(mimes ? ' accept="' + mimes.join(',') + '"' : '') + ' />';
+
+                shimContainer.innerHTML = '<input id="' + I.uid +'" type="file" style="font-size:999px;opacity:0;"' +
+                    (_options.multiple && I.can('select_multiple') ? 'multiple' : '') +
+                    + ' />';
 
 				input = Dom.get(I.uid);
 
